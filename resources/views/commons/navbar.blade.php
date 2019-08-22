f0d411b58d697530f7b3613aae64e7f901103206
s.yusuke <mrs.aoringo@gmail.com>
2019-08-22 10:44:12 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -13,5 +13,11 @@
                 <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
             </ul>
         </div>
+        <ul class="nav navbar-nav navbar-right">
+            <li>{!! link_to_route('signup.get','Signup',[],['class' => 'nav-link']) !!}</li>
+            <li><a href="#">Login</a></li>
+        </ul>
+        
+        
     </nav>
 </header>
